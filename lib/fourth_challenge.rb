e7f4fa18ba800224e6ebddc8bcca88fd3e23c6f4
Stacey Zander <staceyzander@gmail.com>
2019-09-05 18:58:14 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,12 +5,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        benvolio = {
+        {
           :name => "Benvolio",
           :age => "17",
           :attitude => "worried"
         },
-        mercutio = {
+        {
           :name => "Mercutio",
           :age => "18",
           :attitude => "hot-headed"
@@ -22,12 +22,12 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-       steven = {
+        {
           :name => "Steven",
           :age => "30",
           :attitude => "confused"
         },
-        nurse = {
+        {
           :name => "Nurse",
           :age => "44",
           :attitude => "worried"
